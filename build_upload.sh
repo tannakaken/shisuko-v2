3e3d7a660b9f86cf68bfd855a1d8b9a11612a3cf
Tanaka Kensaku <tannakaken@gmail.com>
2024-01-31 11:15:56 +0900

    改行がうまく行かない

--- build_upload.sh
+++ build_upload.sh
@@ -7,15 +7,7 @@ cd ..
 npm install
 npm run build
 
-echo $GITHUB_REPOSITORY
-echo $GITHUB_SHA
-
-res=`curl -L \ 
--H "Accept: application/vnd.github+json" \
--H "Authorization: token $GITHUB_TOKEN" \
--H "X-GitHub-Api-Version: 2022-11-28" \
--X POST "https://api.github.com/repos/tannakaken/${GITHUB_REPOSITORY}/releases" \
--d "
+res=`curl -L -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -X POST "https://api.github.com/repos/tannakaken/${GITHUB_REPOSITORY}/releases" -d "
 {
   \"tag_name\": \"v$GITHUB_SHA\",
   \"target_commitish\": \"$GITHUB_SHA\",
@@ -28,10 +20,4 @@ res=`curl -L \
 assets_url=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["assets_url"])'`
 
 # upload built pdf to release assets
-curl -L \
--H "Accept: application/vnd.github+json" \
--H "Authorization: token $GITHUB_TOKEN" \
--H "X-GitHub-Api-Version: 2022-11-28" \
--H 'Content-Type: application/pdf' \
--X POST "${assets_url}?name=詩晒古_第二号.pdf" \
-  --upload-file '詩晒古_第二号.pdf'
+curl -L -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${assets_url}?name=詩晒古_第二号.pdf" --upload-file '詩晒古_第二号.pdf'
