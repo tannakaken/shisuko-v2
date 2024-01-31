21c4b3af381bca9321c949497e5cc0726ebd9ac0
Tanaka Kensaku <tannakaken@gmail.com>
2024-01-31 11:57:04 +0900

    ファイルの名前を日付にする

--- build_upload.sh
+++ build_upload.sh
@@ -19,7 +19,7 @@ res=`curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorizatio
 # extract asset url
 upload_url=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["upload_url"])'`
 PDF_FILENAME="詩晒古_第二号.pdf"
-upload_url="${upload_url%%\{*}?name=${$GITHUB_SHA}.pdf" # upload_url は 最後に{?name,label} のような文字列がつくため、bashの機能で整形する。
+upload_url="${upload_url%%\{*}?name=${CURRENT_DATETIME}.pdf" # upload_url は 最後に{?name,label} のような文字列がつくため、bashの機能で整形する。
 
 # upload built pdf to release assets
 curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${upload_url}" --upload-file "${PDF_FILENAME}"
