a295647cf673faaf4375a1275e0f4a9fbc4fcdb8
Tanaka Kensaku <tannakaken@gmail.com>
2024-01-31 11:45:23 +0900

    upload_urlを使ってuploadする

--- build_upload.sh
+++ build_upload.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 cd theme-shisuko
 npm install
@@ -17,7 +17,9 @@ res=`curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorizatio
 }"`
 
 # extract asset url
-assets_url=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["assets_url"])'`
+upload_url=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["upload_url"])'`
+PDF_FILENAME="詩晒古_第二号.pdf"
+upload_url="${upload_url%%\{*}?name=${PDF_FILENAME}" # upload_url は 最後に{?name,label} のような文字列がつくため、bashの機能で整形する。
 
 # upload built pdf to release assets
-curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${assets_url}?name=詩晒古_第二号.pdf" --upload-file '詩晒古_第二号.pdf'
+curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${upload_url}" --upload-file "${PDF_FILENAME}"
