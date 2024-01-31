dcdcc6012347b5ad671f88bc53c0ae5bea9b7f94
Tanaka Kensaku <tannakaken@gmail.com>
2024-01-31 11:28:59 +0900

    verboseオプションの追加

--- build_upload.sh
+++ build_upload.sh
@@ -7,7 +7,7 @@ cd ..
 npm install
 npm run build
 
-res=`curl -L -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY}/releases" -d "
+res=`curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY_OWNER}/${GITHUB_REPOSITORY}/releases" -d "
 {
   \"tag_name\": \"v$GITHUB_SHA\",
   \"target_commitish\": \"$GITHUB_SHA\",
@@ -16,9 +16,8 @@ res=`curl -L -H "Accept: application/vnd.github+json" -H "Authorization: token $
   \"prerelease\": false
 }"`
 
-echo $res
 # extract asset url
 assets_url=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["assets_url"])'`
 
 # upload built pdf to release assets
-curl -L -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${assets_url}?name=詩晒古_第二号.pdf" --upload-file '詩晒古_第二号.pdf'
+curl -L --verbose -H "Accept: application/vnd.github+json" -H "Authorization: token $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" -H 'Content-Type: application/pdf' -X POST "${assets_url}?name=詩晒古_第二号.pdf" --upload-file '詩晒古_第二号.pdf'
